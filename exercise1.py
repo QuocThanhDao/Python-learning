4310a191f7bc98db680c436d638d4cab7bc696ad
1453026 <1453026@student.hcmus.edu.vn>
2018-08-31 23:00:34 +0700

    Create file exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,21 @@
+#Create a function to read a file line by line and store it into a list
+def content_list(filename):
+    return
+
+#Create a function to count all number of lines in a file
+def number_of_line(filename):
+    return
+
+#Create a function that return the longest words in the filename
+def longest_word(filename):
+    return
+
+#Create a function returns a list that has the word you've putted in
+#Ex: word_pact("ork") -> [work,pork,fork,...]
+#    word_pact("urn") -> [urn,turn,burn,...]
+def word_pact(string):
+    return
+
+#Create a function that saves the list of the function above into a file named word_pact.txt
+def save_word_pact(word_pact):
+    return
